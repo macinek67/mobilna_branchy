7e4048dae4e612de0a7e8155fc123ee2cd5eba2d
macinek67 <marcindzik1234@gmail.com>
2023-12-08 11:27:53 +0100

    back-end finished

--- mobilna_branchy/mobilna_branchy/MainPage.xaml.cs
+++ mobilna_branchy/mobilna_branchy/MainPage.xaml.cs
@@ -10,19 +10,23 @@ namespace mobilna_branchy
 {
     public partial class MainPage : ContentPage
     {
+        public int likes = 0;
         public MainPage()
         {
             InitializeComponent();
+            lbl_Likes.Text = likes + " likes";
         }
 
         private void Btn_Like_Clicked(object sender, EventArgs e)
         {
-
+            likes++;
+            lbl_Likes.Text = likes + " likes";
         }
 
         private void Btn_Dislike_Clicked(object sender, EventArgs e)
         {
-
+            likes--;
+            lbl_Likes.Text = likes + " likes";
         }
     }
 }
